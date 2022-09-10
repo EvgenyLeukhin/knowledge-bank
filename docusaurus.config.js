12eaff46e062571ce4091524a8284c2b85b432d1
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2022-09-10 17:16:38 +0600

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Knowledge bank',
+  title: 'Knowledge Bank',
   tagline: 'Dinosaurs are cool',
   url: 'https://github.com',
   baseUrl: '/knowledge-bank/',
@@ -59,7 +59,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Knowledge bank',
+        title: 'Knowledge Bank',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
