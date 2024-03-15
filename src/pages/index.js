3df8cd7f8ff2e6e3e6736abc662e78d10bfd345d
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2024-03-15 14:58:06 +0600

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
           <div className={styles.buttons}>
             <Link
               className="button button--primary button--lg"
-              to="/docs/frontend/js/script/"
+              to="/docs/frontend/react/react/"
             >
               Начать читать
             </Link>
